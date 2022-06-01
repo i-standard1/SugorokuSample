7b93a3ab8113e644e3f8722e27af23fb2f3e119b
yutaka <synm.ll603@gmail.com>
2022-06-01 21:13:22 +0900

    move randomly

--- Assets/Button.cs
+++ Assets/Button.cs
@@ -12,25 +12,45 @@ public class Button : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+//         var player = GameObject.Find("Player");
+//         var tf = player.GetComponent<Transform>();
+//         var posY = tf.position;
+//         posY.y += 1.2f;
+//         tf.position = posY;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // if (nextPos > -100) {
-            // var tf = GetComponent<Transform>();
+                // if (nextPos > -100) {
+//             var tf = GetComponent<Transform>();
         var player = GameObject.Find("Player");
         var tf = player.GetComponent<Transform>();
+        
         if(tf.position.x < nextPos) {
             var pos = tf.position;
-            pos.x += 0.1f;
+//             pos.x += 0.1f;
+            pos.x = nextPos;
             tf.position = pos;
         } 
+    }
+    
+    void LastUpdate()
+    {
+//         var player = GameObject.Find("Player");
+//         var tf = player.GetComponent<Transform>();
+//         var pos = tf.position;
+//         pos.y -= 1.2f;
+//         tf.position = pos;
     }
 
     public void OnClick () {
         Debug.Log("押された");
+         var player = GameObject.Find("Player");
+        var tf = player.GetComponent<Transform>();
+        var posY = tf.position;
+        posY.y += 1.2f;
+        tf.position = posY;
         // gameObject.GetComponent<DiceLabel>().Text = 1;
         var dl = (GameObject)GameObject.Find("DiceLabel");
         var rand = new System.Random();
@@ -55,6 +75,9 @@ public class Button : MonoBehaviour
         var ps = target.GetComponent<Transform>();
         var nextX = ps.position.x;
         this.nextPos = nextX;
+        
+        posY.y -= 1.2f;
+        tf.position = posY;
         Debug.Log("nextPos" + nextPos);
 
         // var player = GameObject.Find("Player");
